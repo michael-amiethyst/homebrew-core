1cac06695818e92cadf44ec63161ae54e30936e2
Michael Amiethyst <206824759@tfayd.com>
2025-02-10 10:33:05 -0500

    Added Macos, Linux targets

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,7 +7,12 @@ group = "org.bashpile.core"
 version = "1.0.0"
 
 kotlin {
+    // Multiplatform targets
     jvm("desktop")
+    macosX64()
+    macosArm64()
+    linuxX64()
+    linuxArm64()
     
     sourceSets {
         val desktopMain by getting
