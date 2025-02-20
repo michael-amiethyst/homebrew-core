96d66f710f637fac2cf59fb5d0ed4a5e7575acc3
Michael Amiethyst <206824759@tfayd.com>
2025-02-20 12:15:47 -0500

    0.1.0 - Improved tests for main()

--- src/test/kotlin/org/bashpile/core/MainKtTest.kt
+++ src/test/kotlin/org/bashpile/core/MainKtTest.kt
@@ -1,20 +1,31 @@
 package org.bashpile.core
 
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertNotNull
 import java.io.ByteArrayOutputStream
 import java.io.PrintStream
 
 class MainKtTest {
+    private lateinit var byteArrayOutputStream: ByteArrayOutputStream
+
+    @BeforeEach
+    fun setUp() {
+        byteArrayOutputStream = ByteArrayOutputStream()
+        System.setOut(PrintStream(byteArrayOutputStream))
+    }
+
+    @AfterEach
+    fun tearDown() {
+        System.setOut(System.out)
+    }
+
     @Test
     fun mainWorks() {
-        val byteArrayOutputStream = ByteArrayOutputStream()
-        System.setOut(PrintStream(byteArrayOutputStream))
         main()
-        val output = byteArrayOutputStream.toString().trim()
 
-        assertNotNull(output)
-        assertEquals("Hello World!", output)
+        val output = byteArrayOutputStream.toString()
+        assertEquals("Hello World!\n", output)
     }
 }
