4aa6411ecce403174a6cafa75e507785fbf1ab83
Michael Amiethyst <206824759@tfayd.com>
2025-03-20 12:34:28 -0400

    0.3.0 - Fixed tests and file reading

--- src/main/kotlin/org/bashpile/core/BashpileVisitor.kt
+++ src/main/kotlin/org/bashpile/core/BashpileVisitor.kt
@@ -2,7 +2,10 @@ package org.bashpile.core
 
 /** Converts Antlr AST (aast) to Bashpile AST (bast) */
 class BashpileVisitor: BashpileParserBaseVisitor<BashpileAst>() {
+    // TODO visit children
     override fun visitProgram(antlrAst: BashpileParser.ProgramContext): BashpileAst {
-        return BashpileAst(antlrAst)
+        val firstStatementText: String =
+            if (antlrAst.statement().isNotEmpty()) antlrAst.statement(0).text else "Hello World"
+        return BashpileAst(firstStatementText)
     }
 }
