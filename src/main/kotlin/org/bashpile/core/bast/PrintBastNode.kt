e06dad4c18addeb53f168f9614b7e38d5c0f409b
Michael Amiethyst <206824759@tfayd.com>
2025-04-14 11:48:49 -0400

    0.5.0 - Fixed PrintBastNode, updated tests.  Added failing test for string concat

--- src/main/kotlin/org/bashpile/core/bast/PrintBastNode.kt
+++ src/main/kotlin/org/bashpile/core/bast/PrintBastNode.kt
@@ -3,6 +3,7 @@ package org.bashpile.core.bast
 class PrintBastNode(children: List<BashpileAst>) : BashpileAst(children) {
 
     override fun render(): String {
-        return children.map { it.render() }.joinToString("") + "\n"
+        val childRenders = children.map { it.render() }.joinToString("")
+        return "printf \"$childRenders\\n\""
     }
 }
