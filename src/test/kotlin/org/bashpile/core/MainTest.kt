730e22ca8019c264273f19bc3cf08af1876213ae
Michael Amiethyst <206824759@tfayd.com>
2025-04-14 16:18:39 -0400

    0.5.0 - Refactor of main(), added print boolean test

--- src/test/kotlin/org/bashpile/core/MainTest.kt
+++ src/test/kotlin/org/bashpile/core/MainTest.kt
@@ -3,6 +3,7 @@ package org.bashpile.core
 import com.github.ajalt.clikt.testing.test
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
+import java.io.InputStream
 
 
 class MainTest {
@@ -11,45 +12,55 @@ class MainTest {
         const val HELLO_CONCAT_FILENAME = "src/test/resources/bpsScripts/helloWithConcat.bps"
     }
 
+    val fixture = Main()
+
     @Test
     fun main_withoutScript_printsHelp() {
-        val output = Main().test(arrayOf(""))
+        val output = fixture.test(arrayOf(""))
         assertNotEquals(SCRIPT_SUCCESS, output.statusCode)
         assertTrue(output.stdout.startsWith("Usage:"))
     }
 
     @Test
     fun main_withoutScript_withOption_printsHelp() {
-        val output = Main().test(arrayOf("--name", "Jordi", ""))
+        val output = fixture.test(arrayOf("--name", "Jordi", ""))
         assertNotEquals(SCRIPT_SUCCESS, output.statusCode)
         assertTrue(output.stderr.startsWith("Usage:"))
     }
 
     @Test
     fun main_withScript_works() {
-        val output = Main().test(arrayOf(HELLO_FILENAME))
+        val output = fixture.test(arrayOf(HELLO_FILENAME))
         assertEquals(SCRIPT_SUCCESS, output.statusCode)
         assertEquals("printf \"Hello Bashpile!\\n\"", output.stdout)
     }
 
     @Test
     fun main_withConcatScript_works() {
-        val output = Main().test(arrayOf(HELLO_CONCAT_FILENAME))
+        val output = fixture.test(arrayOf(HELLO_CONCAT_FILENAME))
         assertEquals(SCRIPT_SUCCESS, output.statusCode)
         assertEquals("printf \"Hello Bashpile!\\n\"", output.stdout)
     }
 
     @Test
     fun main_withScript_verbose_works() {
-        val output = Main().test(arrayOf("--verbose=true", HELLO_FILENAME))
+        val output = fixture.test(arrayOf("--verbose=true", HELLO_FILENAME))
         assertEquals(SCRIPT_SUCCESS, output.statusCode)
         assertTrue(output.stdout.endsWith("printf \"Hello Bashpile!\\n\""), "Output: ${output.stdout}")
     }
 
     @Test
     fun main_withBadScript_fails() {
-        val output = Main().test(arrayOf("non_existent_script.bps"))
+        val output = fixture.test(arrayOf("non_existent_script.bps"))
         assertNotEquals(SCRIPT_SUCCESS, output.statusCode)
         assertTrue(output.stdout.startsWith("Usage:"))
+    }
+
+    // getBast tests
+
+    @Test
+    fun getBast_printBool_works() {
+        val printBool: InputStream = "print(true)".byteInputStream()
+        assertEquals("printf \"true\\n\"", fixture.getBast(printBool).render())
     }
 }
