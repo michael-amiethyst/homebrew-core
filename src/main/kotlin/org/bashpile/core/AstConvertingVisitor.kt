747a9e9557f8775a3782e7b95e5a7ad3867753ea
Michael Amiethyst <206824759@tfayd.com>
2025-04-15 11:04:06 -0400

    0.5.0 - Start of type system with String + bool causing exception

--- src/main/kotlin/org/bashpile/core/AstConvertingVisitor.kt
+++ src/main/kotlin/org/bashpile/core/AstConvertingVisitor.kt
@@ -2,7 +2,8 @@ package org.bashpile.core
 
 import org.bashpile.core.BashpileParser.ExpressionContext
 import org.bashpile.core.bast.BashpileAst
-import org.bashpile.core.bast.LiteralBastNode
+import org.bashpile.core.bast.BooleanLiteralBastNode
+import org.bashpile.core.bast.StringLiteralBastNode
 import org.bashpile.core.bast.PrintBastNode
 
 /**
@@ -15,9 +16,18 @@ class AstConvertingVisitor: BashpileParserBaseVisitor<BashpileAst>() {
         val nodes = ctx.expressions().map { visit(it) }
         return PrintBastNode(nodes)
     }
-    
+
     override fun visitLiteral(ctx: BashpileParser.LiteralContext): BashpileAst {
-        return LiteralBastNode(ctx.text)
+        if (ctx.BoolValues() != null) {
+            return BooleanLiteralBastNode(ctx.BoolValues().text.toBoolean())
+        } else if (ctx.NumberValues() != null) {
+            // TODO: Implement number literals
+            throw IllegalArgumentException("Number values are not supported yet")
+        } else if (ctx.StringValues() != null) {
+            return StringLiteralBastNode(ctx.StringValues().text)
+        } else {
+            throw IllegalArgumentException("Unknown literal type")
+        }
     }
 
     /** Encapsulates Antlr context API */
@@ -28,6 +38,8 @@ class AstConvertingVisitor: BashpileParserBaseVisitor<BashpileAst>() {
     override fun visitCalculationExpression(ctx: BashpileParser.CalculationExpressionContext): BashpileAst {
         require(ctx.children.size == 3) { "Calculation expression must have 3 children" }
         require(ctx.children[1].text == "+") { "Only addition is supported" }
+        require(visit(ctx.children[0]) is StringLiteralBastNode) { "Left operand must be a string" }
+        require(visit(ctx.children[2]) is StringLiteralBastNode) { "Right operand must be a string" }
         return BashpileAst(listOf(visit(ctx.children[0]), visit(ctx.children[2])))
     }
 }
