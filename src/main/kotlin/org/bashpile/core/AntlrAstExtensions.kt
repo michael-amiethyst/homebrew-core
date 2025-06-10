29b6ae51a487da3fd806da6ae0cd76e29204872c
Michael Amiethyst <206824759@tfayd.com>
2025-06-10 12:30:51 -0400

    0.8.0 - Factored out AntlrAstExtensions.kt.  Added extension functions.  Updated docs.  Updated executable

---
+++ src/main/kotlin/org/bashpile/core/AntlrAstExtensions.kt
@@ -0,0 +1,36 @@
+package org.bashpile.core
+
+import org.antlr.v4.runtime.tree.TerminalNode
+import org.bashpile.core.BashpileParser.ExpressionContext
+
+
+/** Encapsulates Antlr API to preserve the Law of Demeter */
+fun BashpileParser.PrintStatementContext.expressions(): List<ExpressionContext> {
+    return argumentList().expression()
+}
+
+/** Encapsulates Antlr API to preserve the Law of Demeter */
+fun BashpileParser.VariableDeclarationStatementContext.modifiers(): List<BashpileParser.ModifierContext> {
+    return typedId().modifier()
+}
+
+/** Encapsulates Antlr API to preserve the Law of Demeter */
+fun BashpileParser.VariableDeclarationStatementContext.id(): TerminalNode {
+    return typedId().Id()
+}
+
+/**
+ * Gets the primary (first) type.  E.g., List.
+ * Encapsulates Antlr API to preserve the Law of Demeter
+ */
+fun BashpileParser.VariableDeclarationStatementContext.majorType(): BashpileParser.TypesContext {
+    return typedId().majorType()
+}
+
+/**
+ * Gets the primary (first) type.  E.g., List.
+ * Encapsulates Antlr API to preserve the Law of Demeter
+ */
+fun BashpileParser.TypedIdContext.majorType(): BashpileParser.TypesContext {
+    return complexType().types(0)
+}
--- src/main/kotlin/org/bashpile/core/Main.kt
+++ src/main/kotlin/org/bashpile/core/Main.kt
@@ -20,6 +20,8 @@ import java.io.InputStream
 import java.nio.file.Files
 import java.nio.file.Path
 
+// TODO add Makefile for nativeBuild, jar to Gradle for Formula (Bottles?)
+// TODO change 'int' and 'ref' lexer tokens.  Remove 'unknown'
 
 fun main(args: Array<String>) = Main().main(args)
 
