b7828cee110ea506eaa1d2467a64203b32c3ddc7
Michael Amiethyst <206824759@tfayd.com>
2025-07-23 10:11:18 -0400

    0.9.0 - Trying to fix Ubuntu build

--- src/main/kotlin/org/bashpile/core/bast/types/leaves/ClosingParenthesisLeafBastNode.kt
+++ src/main/kotlin/org/bashpile/core/bast/types/leaves/ClosingParenthesisLeafBastNode.kt
@@ -3,7 +3,7 @@ package org.bashpile.core.bast.types.leaves
 import org.bashpile.core.bast.BastNode
 
 class ClosingParenthesisLeafBastNode : LeafBastNode(")") {
-    override fun replaceChildren(nextChildren: List<BastNode>): LeafBastNode {
+    override fun replaceChildren(nextChildren: List<BastNode>): BastNode {
         return ClosingParenthesisLeafBastNode()
     }
 
--- src/main/kotlin/org/bashpile/core/bast/types/leaves/SubshellStartLeafBastNode.kt
+++ src/main/kotlin/org/bashpile/core/bast/types/leaves/SubshellStartLeafBastNode.kt
@@ -3,7 +3,7 @@ package org.bashpile.core.bast.types.leaves
 import org.bashpile.core.bast.BastNode
 
 class SubshellStartLeafBastNode : LeafBastNode("$(") {
-    override fun replaceChildren(nextChildren: List<BastNode>): LeafBastNode {
+    override fun replaceChildren(nextChildren: List<BastNode>): BastNode {
         return SubshellStartLeafBastNode()
     }
 
