7932a4ab0caf1440fba2765f46be9639253044a4
Michael Amiethyst <206824759@tfayd.com>
2025-08-12 11:32:33 -0400

    0.11.0 - Moved loosenShellStrings to FinishedBastFactory

--- src/main/kotlin/org/bashpile/core/FinishedBastFactory.kt
+++ src/main/kotlin/org/bashpile/core/FinishedBastFactory.kt
@@ -1,19 +1,24 @@
 package org.bashpile.core
 
+import com.google.common.annotations.VisibleForTesting
 import org.apache.logging.log4j.LogManager
 import org.bashpile.core.FinishedBastFactory.Companion.unnestedCount
 import org.bashpile.core.FinishedBastFactory.Companion.unnestedCountLock
+import org.bashpile.core.antlr.AstConvertingVisitor.Companion.ENABLE_STRICT
+import org.bashpile.core.antlr.AstConvertingVisitor.Companion.OLD_OPTIONS
 import org.bashpile.core.bast.BastNode
 import org.bashpile.core.bast.InternalBastNode
 import org.bashpile.core.bast.UnnestTuple
 import org.bashpile.core.bast.expressions.LooseShellStringBastNode
 import org.bashpile.core.bast.expressions.ShellStringBastNode
+import org.bashpile.core.bast.statements.ShellLineBastNode
 import org.bashpile.core.bast.statements.StatementBastNode
 import org.bashpile.core.bast.statements.VariableDeclarationBastNode
 import org.bashpile.core.bast.types.TypeEnum.UNKNOWN
 import org.bashpile.core.bast.types.VariableBastNode
 
 
+@VisibleForTesting
 /** @return An unnested version of the input tree */
 fun BastNode.unnestSubshells(): BastNode {
     synchronized(unnestedCountLock) {
@@ -81,5 +86,40 @@ class FinishedBastFactory {
         logger.info(
             "Mermaid graph after loosing shell strings: {}", looseBast.mermaidGraph())
         return looseBast
+    }
+
+    /** @return A loosened version of the input tree */
+    private fun BastNode.loosenShellStrings(foundLooseShellString: Boolean? = null): Pair<Boolean, BastNode> {
+        val startRecursion = foundLooseShellString == null
+        if (startRecursion) {
+            val looseChildren = children.map { it.loosenShellStrings(false).second }
+            check(looseChildren.isNotEmpty() && looseChildren[0] is StatementBastNode) {
+                "Loose child[0] was not a statement, was " + looseChildren[0].javaClass }
+            return Pair(false, replaceChildren(looseChildren))
+        }
+
+        // recursive call
+
+        val looseResult = children.map {
+            // terminal case is when children are empty
+            it.loosenShellStrings(foundLooseShellString)
+        }.fold(Pair(this is LooseShellStringBastNode, InternalBastNode())) { acc, b ->
+            Pair(acc.first || b.first,
+                acc.second.replaceChildren(acc.second.children + b.second))
+        }
+
+        val foundLoose = looseResult.first
+        val looseStatement = foundLoose && this is StatementBastNode
+        val loosenedChildren = looseResult.second.children
+        val bastNode = if (looseStatement) {
+            // reenable prior (loose) options and then reenable strict mode
+            InternalBastNode(
+                ShellLineBastNode("eval \"$${OLD_OPTIONS}\""),
+                replaceChildren(loosenedChildren),
+                ShellLineBastNode(ENABLE_STRICT))
+        } else {
+            replaceChildren(loosenedChildren)
+        }
+        return Pair(foundLoose, bastNode)
     }
 }
