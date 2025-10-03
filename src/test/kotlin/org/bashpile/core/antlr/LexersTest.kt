70e732761021568372dfed43a272e3b42f6975aa
Michael Amiethyst <206824759@tfayd.com>
2025-10-03 12:32:32 -0400

    0.15.0 - Minor static import/WS changes

--- src/test/kotlin/org/bashpile/core/antlr/LexersTest.kt
+++ src/test/kotlin/org/bashpile/core/antlr/LexersTest.kt
@@ -2,7 +2,7 @@ package org.bashpile.core.antlr
 
 import org.bashpile.core.SCRIPT_SUCCESS
 import org.bashpile.core.runCommand
-import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Order
 import org.junit.jupiter.api.Test
 import java.io.IOException
@@ -14,12 +14,12 @@ class LexersTest {
 
     @Test
     fun cdIsLinuxCommand() {
-        Assertions.assertTrue(Lexers._isLinuxCommand("cd ~"))
+        assertTrue(Lexers._isLinuxCommand("cd ~"))
     }
 
     @Test
     fun awkIsLinuxCommand() {
-        Assertions.assertTrue(
+        assertTrue(
             Lexers._isLinuxCommand(
                 """
                 awk 'BEGIN{RS="\1";ORS="";getline;gsub("\r","");print>ARGV[1]}' filename
@@ -30,7 +30,7 @@ class LexersTest {
 
     @Test
     fun awkWithPreambleIsLinuxCommand() {
-        Assertions.assertTrue(
+        assertTrue(
             Lexers._isLinuxCommand(
                 """
                 a=36 TEST='true' _test4="yes" awk 'BEGIN{RS="\1";ORS="";getline;gsub("\r","");print>ARGV[1]}' filename
@@ -42,7 +42,7 @@ class LexersTest {
 
     @Test
     fun functionIsNotLinuxCommand() {
-        Assertions.assertFalse(Lexers._isLinuxCommand("function times2point5:float(x:float):"))
+        assertFalse(Lexers._isLinuxCommand("function times2point5:float(x:float):"))
     }
 
     @Test
@@ -50,40 +50,40 @@ class LexersTest {
     fun relativeCommandIsLinuxCommand() {
         val command = "$bashDir/my_ls.bash"
         // must be executable to register as a command
-        Assertions.assertEquals(SCRIPT_SUCCESS, "chmod +x $command".runCommand().second)
-        Assertions.assertTrue(Lexers._isLinuxCommand(command))
+        assertEquals(SCRIPT_SUCCESS, "chmod +x $command".runCommand().second)
+        assertTrue(Lexers._isLinuxCommand(command))
     }
 
     @Test
     fun relativeCommandWithArgumentIsLinuxCommand() {
-        Assertions.assertTrue(Lexers._isLinuxCommand("$bashDir/my_ls.bash escapedString.bps"))
+        assertTrue(Lexers._isLinuxCommand("$bashDir/my_ls.bash escapedString.bps"))
     }
 
     @Test
     fun relativeCommandWithDotSlashIsLinuxCommand() {
-        Assertions.assertTrue(Lexers._isLinuxCommand("./$bashDir/my_ls.bash escapedString.bps"))
+        assertTrue(Lexers._isLinuxCommand("./$bashDir/my_ls.bash escapedString.bps"))
     }
 
     @Test
     fun absoluteCommandIsLinuxCommand() {
         val absolutePath = Path.of("./$bashDir/my_ls.bash").toAbsolutePath().toString()
-        Assertions.assertTrue(Lexers._isLinuxCommand(absolutePath), "$absolutePath was not a command")
+        assertTrue(Lexers._isLinuxCommand(absolutePath), "$absolutePath was not a command")
     }
 
     @Test
     fun absoluteCommandWithDashesIsLinuxCommand() {
         val absolutePath = Path.of("./$bashDir/ls-with-dashes.bash-with-dashes")
             .toAbsolutePath().toString()
-        Assertions.assertTrue(Lexers._isLinuxCommand(absolutePath), "$absolutePath was not a command")
+        assertTrue(Lexers._isLinuxCommand(absolutePath), "$absolutePath was not a command")
     }
 
     @Test
     fun elseIfIsNotLinuxCommand() {
-        Assertions.assertFalse(Lexers._isLinuxCommand("else-if check:"), "'else-if check:' was a command")
+        assertFalse(Lexers._isLinuxCommand("else-if check:"), "'else-if check:' was a command")
     }
 
     @Test
     fun printlnIsNotLinuxCommand() {
-        Assertions.assertFalse(Lexers._isLinuxCommand("println"), "'println' was a command")
+        assertFalse(Lexers._isLinuxCommand("println"), "'println' was a command")
     }
 }
